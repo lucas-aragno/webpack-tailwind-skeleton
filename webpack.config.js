91b66875098546ef2ff847a9bfddf465aed22c8a
lucas-aragno <lucas.aragno157@gmail.com>
2020-07-23 16:00:42 -0300

    Push

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
